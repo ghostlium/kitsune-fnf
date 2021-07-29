a78cc0160ab9bf8253ef300b90abdfe97d36f264
Ghost <71734086+ghostlium@users.noreply.github.com>
2021-07-28 21:12:43 -0700

    fixed the thing

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ f = open('data.json',)
 # a dictionary
 data = json.load(f)
 
-glory = []
+glory = [] #the json array
   
 # Iterating through the json
 # list
 for i in data['notes']:
     for j in i['sectionNotes']:
-        glory.append({"type": j[1], "time": j[0]})
+        glory.append({"type": j[1], "time": j[0]+3900})
 
 glory.append({"type": 10, "time": glory[-1]['time']+10})
-print(glory)
 
 with open('app.json', 'w', encoding='utf-8') as f:
     json.dump(glory, f, ensure_ascii=False, indent=4)
 
 # Closing file
 f.close()
+
+#stolen code from online, modified lol
